d32e0760aedfa1fe643af91e2f775e7939d0c7fe
Alexandr <amkazennov99@gmail.com>
2023-11-19 14:56:15 +0300

    was reset

---
+++ LOE/settings/components/database.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_1',
+        'USER': 'db_name_1',
+        'PASSWORD': 'da_password_1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
---
+++ LOE/settings/enviroments/development.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_1',
+        'USER': 'db_name_1',
+        'PASSWORD': 'da_password_1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
